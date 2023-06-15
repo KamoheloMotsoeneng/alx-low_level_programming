61717d6cc725cf6b7ef5f3974ad7d7c036fd1fc8
KamoheloMotsoeneng <motsoenengkamohelo24@gmail.com>
2023-06-15 13:27:39 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.>
 /**
- * main-Entry
- *
+ * main- Entry point
  * Return:Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with propergrammer, but the outcome is a piece of art,\n");
-	return(0);
+	printf'("with propergrammer, but the outcome is a piece of art,\n");
+	return (0)
 }
