a81ac8d7ad6aa9fb405c89a6bbf3cb2976ea47fb
KamoheloMotsoeneng <motsoenengkamohelo24@gmail.com>
2023-06-15 13:48:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.>
+#include<stdio.h>
 /**
- * main- Entry point
- * Return:Always 0 (Success)
+ * main - Entry point
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf'("with propergrammer, but the outcome is a piece of art,\n");
-	return (0)
+	printtf'("with proper grammar, but the outcome is a piece of art,\n);
+	return (0);
 }
+
